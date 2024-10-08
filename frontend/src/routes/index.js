e47be8522be8e9903a1963635bbac43b076030aa
imprince-kushwaha <mijadon593@hidelux.com>
2024-10-08 21:45:24 +0530

    remove /

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
             //     element:<ForgotPassword/>
             // },
             {
-                path: "/chat",
+                path: "chat",
                 element: <Home />
                 ,
                 children: [{
