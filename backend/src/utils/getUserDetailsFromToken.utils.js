e9f993ccc97d7bf35324a0c607ce6be1fe984013
imprince-kushwaha <mijadon593@hidelux.com>
2024-10-08 16:15:02 +0530

    added console

--- backend/src/utils/getUserDetailsFromToken.utils.js
+++ backend/src/utils/getUserDetailsFromToken.utils.js
@@ -1,33 +1,36 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js"
 
-const getUserDetailsFromToken=async (token)=>{
-    if(!token){
+const getUserDetailsFromToken = async (token) => {
+    console.log("token",token)
+    if (!token) {
         return {
-            message:"Session Out",
-            logout:true,
-        }
-    }
-    // const decode=await jwt.verify(token,process.env.JWT_SECRET_KEY)
-    // const user=await User.findById(decode.id).select("-password")
-    // return user
-    try {
-        const decode = await jwt.verify(token, process.env.JWT_SECRET_KEY);
-        const user = await User.findById(decode.id).select("-password");
-        if (!user) {
-            return {
-                message: "User not found",
-                logout: true,
-            };
-        }
-        return user;
-    } catch (error) {
-        // Catch the error and return appropriate message
-        return {
-            message: "Invalid token, session expired",
+            message: "Session Out",
             logout: true,
-        };
+        }
     }
+    const decode=await jwt.verify(token,process.env.JWT_SECRET_KEY)
+    const user = await User.findById(decode.id).select("-password")
+    return user
+
+    // try {
+    //     const decode = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+    //     const user = await User.findById(decode.id).select("-password");
+    //     if (!user) {
+    //         return {
+    //             message: "User not found",
+    //             logout: true,
+    //         };
+    //     }
+    //     return user;
+    // } catch (error) {
+    //     // Catch the error and return appropriate message
+    //     return {
+    //         message: "Invalid token, session expired",
+    //         logout: true,
+    //     };
+    // }
+
 }
 
 export default getUserDetailsFromToken
