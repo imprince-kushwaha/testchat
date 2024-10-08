e8dd2102fc70886acb9935454b75e1e316f88c72
imprince-kushwaha <mijadon593@hidelux.com>
2024-10-08 15:06:50 +0530

    new type axios passs

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -131,8 +131,11 @@ const Home = () => {
     const fetchUserDetails = async () => {
         try {
             const URL = ` ${process.env.REACT_APP_BACKEND_URL}/api/user-details`;
-            const response = await axios({
-                url: URL,
+            // const response = await axios({
+            //     url: URL,
+            //     withCredentials: true
+            // });
+            const response = await axios.get(URL, {
                 withCredentials: true
             });
             dispatch(setUser(response.data.data));
